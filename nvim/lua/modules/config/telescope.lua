55dae207487cdaa711ce6f5bc7319a7342eb49c1
Mark Skelton <mark.skelton@acquia.com>
2022-06-15 14:36:03 -0500

    C-k/j again

--- nvim/lua/modules/config/telescope.lua
+++ nvim/lua/modules/config/telescope.lua
@@ -9,6 +9,8 @@ return function()
 			mappings = {
 				i = {
 					["<esc>"] = actions.close,
+					["<C-j>"] = actions.move_selection_next,
+					["<C-k>"] = actions.move_selection_previous,
 					["<C-u>"] = false, -- Clear prompt with C-u
 				},
 			},
