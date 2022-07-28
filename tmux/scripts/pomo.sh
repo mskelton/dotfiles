e6f28b79682d2c1dbd1381991310f9cd4d225171
Mark Skelton <mdskelton99@gmail.com>
2022-07-27 22:53:10 -0500

    Update pomodoro config for Tmux

---
+++ tmux/scripts/pomo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+status=$(pomo)
+
+# Prints the pomodoro status with a more obvious background when in tmux. If 
+# there is no active session, this will not print anything.
+if [[ ! -z "$status" ]]; then
+  echo "#[fg=#7aa2f7]#[fg=#1D202F,bg=#7aa2f7] $status "
+fi
