da2407e02b82ba9ef7227b93c8219e1938e7adc7
Mark Skelton <mark.skelton@acquia.com>
2022-09-08 08:57:25 -0500

    Fix keybindings

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,15 @@ for file in $HOME/.config/fish/widen.fish $HOME/.config/fish/env.fish
   test -f $file; and source $file
 end
 
+function fish_hybrid_key_bindings
+  fzf_key_bindings
+  fish_default_key_bindings -M insert
+  fish_vi_key_bindings --no-erase
+end
+
+# Use my custom keybindings
+set -g fish_key_bindings fish_hybrid_key_bindings
+
 # Environment variables
 set -x EDITOR "nvim"
 set -x GOPATH "$HOME/go"
--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-function fish_user_key_bindings
-  fzf_key_bindings
-  fish_default_key_bindings -M insert
-  fish_vi_key_bindings --no-erase
-end
