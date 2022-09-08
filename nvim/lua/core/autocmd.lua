df7dae21878de5e3ba815cd83118e6a862857712
Mark Skelton <mdskelton99@gmail.com>
2022-09-07 22:25:54 -0500

    Auto reload keymap

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -1,11 +1,7 @@
 vim.cmd([[
-  " augroup PackerCompile
-  "   au!
-  "   au BufWritePost */nvim/lua/plugins.lua,*/nvim/lua/config/*.lua source <afile> | PackerCompile
-  " augroup END 
-
-  augroup FormatOptions
+  augroup MarkSkelton
     au!
     au BufEnter * set formatoptions-=c | set formatoptions-=o
+    au BufWritePost */nvim/lua/core/keymap.lua source <afile>
   augroup END 
 ]])
