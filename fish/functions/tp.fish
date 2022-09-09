c9724b294707b5317c6736b0614c5b1d145b98cf
Mark Skelton <mdskelton99@gmail.com>
2022-09-09 08:06:15 -0500

    Auto switch tmux sessions

--- fish/functions/tp.fish
+++ fish/functions/tp.fish
@@ -1,6 +1,10 @@
 function tp -a dir name -d 'Create or connect to a tmux session'
   set -q dir[1]; or set dir (basename (pwd))
-  set -q name[1]; or set name $dir
 
-  tmux new -A -c ~/dev/$dir -s $name
+  if test -n "$TMUX"
+    tmux switch -t $dir
+  else 
+    set -q name[1]; or set name $dir
+    tmux new -A -c ~/dev/$dir -s $name
+  end
 end
