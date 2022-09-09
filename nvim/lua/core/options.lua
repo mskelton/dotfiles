27af724b6f4304a1cf4f2cff119016786a7bc0d1
Mark Skelton <mdskelton99@gmail.com>
2022-09-08 22:45:16 -0500

    Better split direction

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -13,20 +13,26 @@ o.expandtab = true
 o.guicursor = o.guicursor + "c:ver25"
 o.hlsearch = false
 o.ignorecase = true
-o.list = true
-o.listchars = { tab = "  ", trail = "·" }
 o.mouse = "a"
 o.number = true
 o.relativenumber = true
 o.shiftwidth = 2
 o.shortmess = o.shortmess + "I"
+o.showbreak = "    "
 o.signcolumn = "yes"
 o.smartcase = true
 o.tabstop = 2
 o.termguicolors = true
 o.undofile = true
 o.updatetime = 100
-o.showbreak = "    "
+
+-- Show trailing spaces
+o.list = true
+o.listchars = { tab = "  ", trail = "·" }
+
+-- Better split directions
+o.splitright = true
+o.splitbelow = true
 
 -- Abbreviations
 vim.cmd("ca <expr> %% expand('%:p:h')")
