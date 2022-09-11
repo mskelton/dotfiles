2b9535943cb778cf965354e955f3be762c5ba7db
Mark Skelton <mdskelton99@gmail.com>
2022-09-11 17:51:00 -0500

    tsconfig jsonc

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -1,8 +1,18 @@
 vim.cmd([[
   augroup mskelton
     au!
+
+    " Disable continuation comments when using o/O. CR will still add the
+    " comment leader.
     au BufEnter * set formatoptions-=c | set formatoptions-=o
+
+    " Auto source
     au BufWritePost */nvim/lua/core/*.lua source <afile>
+
+    " Enable spelling in markdown files
     au FileType markdown setlocal spell spelllang=en_us
+
+    " TS config files use jsonc sntax
+    au BufEnter,BufNew tsconfig*.json set ft=jsonc
   augroup END
 ]])
