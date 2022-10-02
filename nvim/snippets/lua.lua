a295d81db09b0dcdf33de56dc27e7a9b19960b85
Mark Skelton <mdskelton99@gmail.com>
2022-10-02 09:11:45 -0500

    Improve lua snips

--- nvim/snippets/lua.lua
+++ nvim/snippets/lua.lua
@@ -1,13 +1,13 @@
 ---@diagnostic disable: undefined-global
 
 return {
-	parse("fn", "local $1 = function($2)\n\t$0\nend"),
+	parse("fn", "local function $1($2)\n\t$0\nend"),
 	parse("mod", "local M = {}\n\nM.$0\n\nreturn M"),
 	s(
 		"req",
 		fmt('local {} = require("{}")', {
 			f(function(import_name)
-				local parts = vim.split(import_name[1][1], ".", true)
+				local parts = vim.split(import_name[1][1], ".", { plain = true })
 				return parts[#parts] or ""
 			end, { 1 }),
 			i(1),
