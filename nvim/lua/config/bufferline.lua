db8b68722eb4c407c71bd71092111c6a1d2040bd
Mark Skelton <mdskelton99@gmail.com>
2022-10-07 08:33:47 -0500

    Back to thin

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -2,7 +2,7 @@ return function()
 	require("bufferline").setup({
 		options = {
 			indicator = { style = "none" },
-			separator_style = "slant",
+			separator_style = "thin",
 			show_buffer_close_icons = false,
 		},
 	})
