b2ed4bfd0f58db05cc59f109dbd44d328c231136
Mark Skelton <mdskelton99@gmail.com>
2022-10-10 18:24:46 -0500

    Only run Prettier when configured in the repo

--- nvim/lua/config/lsp/null-ls.lua
+++ nvim/lua/config/lsp/null-ls.lua
@@ -2,7 +2,23 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.prettierd.with({
+			condition = function(utils)
+				return utils.root_has_file({
+					-- https://prettier.io/docs/en/configuration.html
+					".prettierrc",
+					".prettierrc.json",
+					".prettierrc.yml",
+					".prettierrc.yaml",
+					".prettierrc.json5",
+					".prettierrc.js",
+					".prettierrc.cjs",
+					".prettier.config.js",
+					".prettier.config.cjs",
+					".prettierrc.toml",
+				})
+			end,
+		}),
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.swiftformat,
 	},
