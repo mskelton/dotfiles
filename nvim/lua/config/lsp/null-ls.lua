520fe40adfcbe4b765528c4f36e3a9e314df9e67
Mark Skelton <mark.skelton@acquia.com>
2022-10-11 17:41:47 -0500

    Back to regular Prettier setup

--- nvim/lua/config/lsp/null-ls.lua
+++ nvim/lua/config/lsp/null-ls.lua
@@ -2,23 +2,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettierd.with({
-			condition = function(utils)
-				return utils.root_has_file({
-					-- https://prettier.io/docs/en/configuration.html
-					".prettierrc",
-					".prettierrc.json",
-					".prettierrc.yml",
-					".prettierrc.yaml",
-					".prettierrc.json5",
-					".prettierrc.js",
-					".prettierrc.cjs",
-					".prettierrc.toml",
-					"prettier.config.js",
-					"prettier.config.cjs",
-				})
-			end,
-		}),
+		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.swiftformat,
 	},
