ca787514c380a5d295ac0243eb30e18f511e63d6
Mark Skelton <mark.skelton@acquia.com>
2022-10-20 10:09:55 -0500

    Update bob

--- fish/completions/bob.fish
+++ fish/completions/bob.fish
@@ -1,5 +1,5 @@
 function __bob_list_scripts
-  ls $HOME/dev/bob/*.fish | xargs basename | sed 's/.fish//'
+  ls $HOME/dev/bob/bin/* | xargs basename
 end
 
 complete -c bob -f -n "__fish_use_subcommand" -a '(__bob_list_scripts)'
