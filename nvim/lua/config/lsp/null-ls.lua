c68197dfc0d7a90bcd57fdde7565840361605290
Mark Skelton <mdskelton99@gmail.com>
2022-11-25 09:25:49 -0600

    Only use local Prettier

--- nvim/lua/config/lsp/null-ls.lua
+++ nvim/lua/config/lsp/null-ls.lua
@@ -2,7 +2,11 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.prettierd.with({
+			env = {
+				PRETTIERD_LOCAL_PRETTIER_ONLY = true,
+			},
+		}),
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.swiftformat,
