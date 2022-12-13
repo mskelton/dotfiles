cc807d58b9a903e945ef296c0e2a28b027e86212
Mark Skelton <mark.skelton@acquia.com>
2022-12-13 14:53:36 -0600

    Update tmux completions

--- fish/completions/tp.fish
+++ fish/completions/tp.fish
@@ -1,8 +1,6 @@
 function __tmux_complete_sessions
-  if test -n "$TMUX"
-    set sessions (command tmux list-sessions -F "#{session_name}")
-    printf "%s\tSession\n" $sessions
-  end
+  set sessions (command tmux list-sessions -F "#{session_name}" 2> /dev/null)
+  printf "%s\tSession\n" $sessions
 end
 
 complete -c tp -fk -n "__fish_use_subcommand" -a "(__fish_complete_directories)"
