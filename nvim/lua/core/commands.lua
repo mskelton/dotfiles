190268239688d67a8aced675c8f288eb9015712f
Mark Skelton <mdskelton99@gmail.com>
2023-01-05 12:15:42 -0600

    Add E command

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -3,7 +3,12 @@ vim.api.nvim_create_user_command("Pomo", "silent !pomo <args>", { nargs = "*" })
 vim.api.nvim_create_user_command("Task", "!task <args>", { nargs = "*" })
 
 -- Create new file in current directory
--- TODO
+vim.api.nvim_create_user_command("E", "e %:h/<args>", {
+	nargs = 1,
+	complete = function(_, _, _)
+		return { "1", "2", "3" }
+	end,
+})
 
 -- Format/unformat files with JQ
 vim.api.nvim_create_user_command("FormatJson", ":%!jq .", {})
