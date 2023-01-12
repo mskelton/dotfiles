27ac32121823ea5c363414c162c559a6a0436d0e
Mark Skelton <mdskelton99@gmail.com>
2023-01-11 23:23:03 -0600

    Default shortcuts

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -25,6 +25,38 @@ defaults write -g com.apple.keyboard.fnState -bool true
 defaults write com.apple.Dock show-recents -int 0
 defaults write com.apple.Dock static-others '()'
 
+# Shortcuts
+disable_shortcut="<dict><key>enabled</key><false/></dict>"
+
+# Disable spotlight search
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 $disable_shortcut
+
+# Disable screenshot shortcuts to allow Shottr to use them
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 28 $disable_shortcut
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 30 $disable_shortcut
+
+# Disable input source switching, it conflicts with Neovim autocompletion
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 61 $disable_shortcut
+
+# Add Option+D for toggling do not disturb
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 175 "
+  <dict>
+    <key>enabled</key>
+    <true/>
+    <key>value</key>
+    <dict>
+      <key>type</key>
+      <string>standard</string>
+      <key>parameters</key>
+      <array>
+        <integer>100</integer>
+        <integer>2</integer>
+        <integer>524288</integer>
+      </array>
+    </dict>
+  </dict>
+"
+
 # Disable disk not ejected properly notification. This notification is very
 # annoying when flashing firmware to the Advantage 360 keyboard.
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool true
