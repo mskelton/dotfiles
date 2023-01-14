fe31fd264e82264296ff6da9b03bbbd18907b068
Mark Skelton <mdskelton99@gmail.com>
2023-01-14 17:26:36 -0600

    Check if there are references before sending to qfix list

--- nvim/lua/config/lsp/handlers.lua
+++ nvim/lua/config/lsp/handlers.lua
@@ -6,21 +6,27 @@ M.register_handlers = function()
 	-- Language servers often include the current line in find references calls
 	-- which is really unnecessary since I know there is a reference where my
 	-- cursor is. This filters out the current line before sending the results to
-	-- the quick fix list.
+	-- the quickfix list.
 	vim.lsp.handlers["textDocument/references"] = vim.lsp.with(find_references, {
 		on_list = function(args)
-			local cursorLine = unpack(vim.api.nvim_win_get_cursor(0))
+			local cursor_line = unpack(vim.api.nvim_win_get_cursor(0))
 			local items = vim.tbl_filter(function(item)
-				return item.lnum ~= cursorLine
+				return item.lnum ~= cursor_line
 			end, args.items)
 
-			vim.fn.setqflist(
-				{},
-				" ",
-				{ title = args.title, items = items, context = args.ctx }
-			)
+			if vim.tbl_isempty(items) then
+				-- Since we filter the results after the internal LSP no-results check
+				-- we have to re-check if there are no results.
+				vim.notify("No references found")
+			else
+				vim.fn.setqflist(
+					{},
+					" ",
+					{ title = args.title, items = items, context = args.ctx }
+				)
 
-			vim.api.nvim_command("botright copen")
+				vim.api.nvim_command("botright copen")
+			end
 		end,
 	})
 end
