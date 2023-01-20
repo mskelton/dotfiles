5cec77be2bae47290cd1d0e23f8e68c2de7ed0a0
Mark Skelton <mdskelton99@gmail.com>
2023-01-19 22:07:23 -0600

    Move comment

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -8,37 +8,6 @@ return {
 	"fladson/vim-kitty",
 	"kdheepak/lazygit.nvim",
 	{
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-		config = function()
-			local js_comments = {
-				__default = "// %s",
-				jsx_element = "{/* %s */}",
-				jsx_fragment = "{/* %s */}",
-				jsx_attribute = "// %s",
-				comment = "// %s",
-			}
-
-			require("nvim-treesitter.configs").setup({
-				context_commentstring = {
-					enable = true,
-					enable_autocmd = false,
-					config = {
-						javascript = js_comments,
-						typescript = js_comments,
-						tsx = js_comments,
-					},
-				},
-			})
-
-			require("Comment").setup({
-				pre_hook = require(
-					"ts_context_commentstring.integrations.comment_nvim"
-				).create_pre_hook(),
-			})
-		end,
-	},
-	{
 		"windwp/nvim-autopairs",
 		config = true,
 	},
