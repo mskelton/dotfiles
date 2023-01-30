71229bf6cce24179d163ec57ca0037e7427d9738
Mark Skelton <mdskelton99@gmail.com>
2023-01-29 22:47:22 -0600

    pnpm

---
+++ fish/completions/pnpm.fish
@@ -0,0 +1,21 @@
+function __pnpm_completion
+  set cmd (commandline -o)
+  set cursor (commandline -C)
+  set words (count $cmd)
+
+  set completions (eval env DEBUG=\"" \"" COMP_CWORD=\""$words\"" COMP_LINE=\""$cmd \"" COMP_POINT=\""$cursor\"" pnpm completion -- $cmd)
+
+  if [ "$completions" = "__tabtab_complete_files__" ]
+    set -l matches (commandline -ct)*
+
+    if [ -n "$matches" ]
+      __fish_complete_path (commandline -ct)
+    end
+  else
+    for completion in $completions
+      echo -e $completion
+    end
+  end
+end
+
+complete -f -d 'pnpm' -c pnpm -a "(__pnpm_completion)"
