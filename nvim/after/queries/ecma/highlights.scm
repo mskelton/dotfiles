bfd721a14d56eea7f3726b9c4eecc612c85f2549
Mark Skelton <mark.skelton@acquia.com>
2023-02-17 14:05:04 -0600

    Fix emmet

--- nvim/after/queries/ecma/highlights.scm
+++ nvim/after/queries/ecma/highlights.scm
@@ -8,3 +8,29 @@
 
 ; Highlight template string symbols
 (template_string "`" @string)
+
+; Styled components. This is required to make emmet work properly inside
+; styled component strings.
+
+; styled.div`<css>`
+(call_expression
+ function: (member_expression
+   object: (identifier) @_name
+     (#eq? @_name "styled"))
+ arguments: ((template_string) @styled
+  (#offset! @styled 0 1 0 -1)))
+
+; styled(Component)`<css>`
+(call_expression
+ function: (call_expression
+   function: (identifier) @_name
+     (#eq? @_name "styled"))
+ arguments: ((template_string) @styled
+  (#offset! @styled 0 1 0 -1)))
+
+; css`<css>`, keyframes`<css>`
+(call_expression
+  function: (identifier) @_name
+    (#match? @_name "^(css|keyframes)")
+  arguments: ((template_string) @styled
+  (#offset! @styled 0 1 0 -1)))
