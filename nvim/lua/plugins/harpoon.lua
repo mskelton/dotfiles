15cdc7826d386ce9b837a29187ae22833d001820
Mark Skelton <mdskelton99@gmail.com>
2023-02-18 10:26:30 -0600

    Harpoon file

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,38 @@
+return {
+	"ThePrimeagen/harpoon",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{
+			"<leader>ho",
+			function()
+				require("harpoon.ui").toggle_quick_menu()
+			end,
+			mode = { "n", "v" },
+			desc = "Open Harpoon menu",
+		},
+		{
+			"<leader>ha",
+			function()
+				require("harpoon.mark").add_file()
+			end,
+			mode = { "n", "v" },
+			desc = "Add file to Harpoon",
+		},
+		{
+			"]h",
+			function()
+				require("harpoon.ui").nav_next()
+			end,
+			mode = "n",
+			desc = "Next harpoon mark",
+		},
+		{
+			"[h",
+			function()
+				require("harpoon.ui").nav_prev()
+			end,
+			mode = "n",
+			desc = "Previous harpoon mark",
+		},
+	},
+}
