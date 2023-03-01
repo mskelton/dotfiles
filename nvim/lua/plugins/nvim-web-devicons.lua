94de09def3ac80c05a620d6f09c4116c0b0b5c1e
Mark Skelton <mdskelton99@gmail.com>
2023-03-01 16:32:01 -0500

    Update icons

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -2,12 +2,6 @@ return {
 	"mskelton/nvim-web-devicons",
 	dependencies = { "mskelton/termicons.nvim" },
 	config = function()
-		local termicons = require("termicons")
-
-		require("nvim-web-devicons").setup({
-			override = termicons.get_overrides(),
-			override_by_extension = termicons.get_overrides_by_extension(),
-			override_by_filename = termicons.get_overrides_by_filename(),
-		})
+		require("termicons").setup()
 	end,
 }
