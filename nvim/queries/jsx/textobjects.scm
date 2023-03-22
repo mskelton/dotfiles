4d4689e61a8449913c9cb7cdc5e22a3ce8be9f48
Mark Skelton <mark@federato.ai>
2023-03-22 18:39:23 -0500

    Fix JSX textobject

--- nvim/queries/jsx/textobjects.scm
+++ nvim/queries/jsx/textobjects.scm
@@ -1,10 +1,10 @@
 ; inherits: ecma
 
-; JSX tags
-(jsx_element) @tag.outer
+; JSX elements
 (jsx_self_closing_element) @tag.outer @tag.inner
-((jsx_element open_tag: (_) . (_) @_child . close_tag: (_))
- (#make-range! "tag.inner" @_child @_child))
+
+(jsx_element) @tag.outer
+(jsx_element open_tag: (_) . (_) @tag.inner . close_tag: (_))
 ((jsx_element open_tag: (_) . (_) @_start (_) @_end . close_tag: (_))
  (#make-range! "tag.inner" @_start @_end))
 
