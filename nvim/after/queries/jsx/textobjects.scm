4637fe822a282dc76b93aad4fd0050963059e7e4
Mark Skelton <mdskelton99@gmail.com>
2023-03-22 07:49:33 -0500

    Better JSX text objects and param object
    Fixes #109

--- nvim/after/queries/jsx/textobjects.scm
+++
@@ -1,6 +0,0 @@
-; extends
-
-; JSX attributes
-(jsx_attribute) @tag.attribute.outer
-(jsx_attribute (jsx_expression (_) @tag.attribute.inner))
-(jsx_attribute (string (string_fragment) @tag.attribute.inner))
---
+++ nvim/queries/jsx/textobjects.scm
@@ -0,0 +1,12 @@
+; inherits: ecma
+
+; JSX tags
+(jsx_element) @tag.outer
+(jsx_self_closing_element) @tag.outer @tag.inner
+((jsx_element open_tag: (_) . (_) @_start (_) @_end . close_tag: (_))
+ (#make-range! "tag.inner" @_start @_end))
+
+; JSX attributes
+(jsx_attribute) @parameter.outer
+(jsx_attribute (jsx_expression (_) @parameter.inner))
+(jsx_attribute (string (string_fragment) @parameter.inner))
