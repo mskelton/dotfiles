ddf7a80601c27cafa6d5cd524a464f8a6dda29fa
Mark Skelton <mdskelton99@gmail.com>
2023-03-26 08:49:05 -0500

    Fix inner tag text object

--- nvim/queries/jsx/textobjects.scm
+++ nvim/queries/jsx/textobjects.scm
@@ -3,6 +3,8 @@
 ; JSX tags
 (jsx_element) @tag.outer
 (jsx_self_closing_element) @tag.outer @tag.inner
+((jsx_element open_tag: (_) . (_) @_child . close_tag: (_))
+ (#make-range! "tag.inner" @_child @_child))
 ((jsx_element open_tag: (_) . (_) @_start (_) @_end . close_tag: (_))
  (#make-range! "tag.inner" @_start @_end))
 
