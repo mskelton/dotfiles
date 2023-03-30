323f2bb49d750f85ee6aa1c16899f33266e3a820
Mark Skelton <mark@federato.ai>
2023-03-30 16:14:06 -0500

    Disable traces for now

--- nvim/lua/plugins/abolish.lua
+++ nvim/lua/plugins/abolish.lua
@@ -6,7 +6,9 @@ return {
 	{
 		"markonm/traces.vim",
 		event = "VeryLazy",
+		enabled = false,
 		init = function()
+			vim.g.traces_substitute_preview = 1
 			vim.g.traces_abolish_integration = 1
 			vim.api.nvim_set_hl(0, "TracesSearch", { link = "IncSearch" })
 		end,
