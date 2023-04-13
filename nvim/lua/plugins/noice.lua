182c47e55ae61513f6c6a0853184f726988fbe74
Mark Skelton <mdskelton99@gmail.com>
2023-04-12 22:10:39 -0500

    Add Noice

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,24 @@
+return {
+	"folke/noice.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	lazy = false,
+	enabled = false,
+	config = {
+		cmdline = {
+			view = "cmdline",
+		},
+		messages = {
+			enabled = false,
+		},
+		lsp = {
+			progress = {
+				enabled = false,
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+	},
+}
