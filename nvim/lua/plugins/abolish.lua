bf77e423658a3339e1050c5b282cb6302b02d35f
Mark Skelton <mdskelton99@gmail.com>
2023-04-20 21:06:33 -0500

    More common typos

--- nvim/lua/plugins/abolish.lua
+++ nvim/lua/plugins/abolish.lua
@@ -4,9 +4,31 @@ return {
 		cmd = { "Abolish", "Subvert" },
 		lazy = false,
 		config = function()
-			vim.cmd.Abolish("teh", "the")
-			vim.cmd.Abolish("deductable", "deductible")
+			vim.cmd.Abolish("accomodate", "accommodate")
+			vim.cmd.Abolish("accross", "across")
+			vim.cmd.Abolish("appologize", "apologize")
 			vim.cmd.Abolish("calender", "calendar")
+			vim.cmd.Abolish("committment", "commitment")
+			vim.cmd.Abolish("concious", "conscious")
+			vim.cmd.Abolish("deductable", "deductible")
+			vim.cmd.Abolish("definately", "definitely")
+			vim.cmd.Abolish("embarass{,ment}", "embarrass{,ment}")
+			vim.cmd.Abolish("enviroment", "environment")
+			vim.cmd.Abolish("existance", "existence")
+			vim.cmd.Abolish("grammer", "grammar")
+			vim.cmd.Abolish("independant", "independent")
+			vim.cmd.Abolish("neccessary", "necessary")
+			vim.cmd.Abolish("occassion", "occasion")
+			vim.cmd.Abolish("occured", "occurred")
+			vim.cmd.Abolish("occurence", "occurrence")
+			vim.cmd.Abolish("posession", "possession")
+			vim.cmd.Abolish("priviledge", "privilege")
+			vim.cmd.Abolish("reciept", "receipt")
+			vim.cmd.Abolish("recieve", "receive")
+			vim.cmd.Abolish("relevent", "relevant")
+			vim.cmd.Abolish("seperate", "separate")
+			vim.cmd.Abolish("teh", "the")
+			vim.cmd.Abolish("wierd", "weird")
 		end,
 	},
 	{
