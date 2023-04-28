fc33e4ddc3d821c50949cebdc25660dc908d3164
Mark Skelton <mdskelton99@gmail.com>
2023-04-27 23:03:23 -0500

    Update colors

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -5,13 +5,21 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			on_highlights = function(hl, c)
-				hl["@function.call"] = { fg = c.blue, bold = true }
-				hl["@method.call"] = { fg = c.blue, bold = true }
-				hl["@keyword.return"] = { fg = c.purple, italic = true }
 				hl["@constructor"] = { fg = c.red }
 				hl["@tag"] = { fg = c.red }
 				hl["@tag.attribute"] = { fg = c.purple }
 				hl["@type.builtin"] = { fg = c.blue1 }
+
+				-- Blue bold for functions
+				hl["@function.call"] = { fg = c.blue, bold = true }
+				hl["@function.macro"] = { fg = c.blue, bold = true }
+				hl["@method.call"] = { fg = c.blue, bold = true }
+
+				-- Italic purple for keyword type things
+				hl["@keyword.function"] = { fg = c.purple, italic = true }
+				hl["@keyword.return"] = { fg = c.purple, italic = true }
+				hl.Statement = { fg = c.purple, italic = true }
+				hl.PreProc = { fg = c.purple, italic = true }
 
 				-- Make line numbers easier to read
 				hl.LineNr = { fg = c.blue }
