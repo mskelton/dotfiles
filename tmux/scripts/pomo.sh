684ad5802a72cfef2fb31ce609285c0e6136e882
Mark Skelton <mdskelton99@gmail.com>
2023-04-29 06:53:48 -0500

    Prefer cargo binaries

--- tmux/scripts/pomo.sh
+++ tmux/scripts/pomo.sh
@@ -2,8 +2,8 @@
 
 status=$(pomo)
 
-# Prints the pomodoro status with a more obvious background when in tmux. If 
+# Prints the pomodoro status with a more obvious background when in tmux. If
 # there is no active session, this will not print anything.
 if [[ ! -z "$status" ]]; then
-  echo "#[fg=#7aa2f7]#[fg=#1D202F,bg=#7aa2f7] $status "
+	echo "#[fg=#7aa2f7]#[fg=#1D202F,bg=#7aa2f7] $status "
 fi
