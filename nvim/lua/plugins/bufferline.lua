6b87da3fed2b339a2cfff6b44506f1cc66f512ca
Mark Skelton <mark@federato.ai>
2023-05-15 11:55:26 -0500

    Fix formatting issue, it was bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -5,7 +5,6 @@ return {
 	config = true,
 	opts = {
 		options = {
-			always_show_bufferline = false,
 			indicator = { style = "none" },
 			separator_style = "thin",
 			show_buffer_close_icons = false,
