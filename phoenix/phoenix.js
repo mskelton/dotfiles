e62ca338a3b80d26de9778c3c78b5219950961c9
Mark Skelton <mdskelton99@gmail.com>
2023-05-28 09:10:40 -0500

    Add some shortcuts

--- phoenix/phoenix.js
+++ phoenix/phoenix.js
@@ -3,6 +3,37 @@ Phoenix.set({
   openAtLogin: true,
 })
 
-const handler = new Key("k", ["cmd", "option"], () => {
-  Window.focused().setTopLeft({ x: 100, y: 10 })
+function split(leftWindows, rightWindows) {
+  const screen = Screen.main().flippedVisibleFrame()
+  const width = screen.width * 0.5
+  const args = {
+    y: screen.y,
+    width: width,
+    height: screen.height,
+  }
+
+  leftWindows.forEach((window) => {
+    window.setFrame({ ...args, x: screen.x })
+  })
+
+  rightWindows.forEach((window) => {
+    window.setFrame({ ...args, x: screen.x + width })
+  })
+}
+
+// Split browser and terminal
+Key.on("k", ["cmd", "option"], () => {
+  const arc = App.get("Arc")
+  const kitty = App.get("kitty")
+
+  split(arc ? arc.windows() : [], kitty ? kitty.windows() : [])
+})
+
+// Maximize everything
+Key.on("l", ["cmd", "option"], () => {
+  App.all().forEach((app) => {
+    app.windows().forEach((window) => {
+      window.maximise()
+    })
+  })
 })
