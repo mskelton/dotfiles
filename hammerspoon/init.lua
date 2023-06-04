3bc9cb3578fbd6e0f41d7cd3e070df78b92daab5
Mark Skelton <mdskelton99@gmail.com>
2023-06-04 17:31:37 -0500

    Comments

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,3 +16,9 @@ LiveReloadWatcher = hs.pathwatcher.new(config_path, LiveReload):start()
 -- hs.hotkey.bind({ "cmd", "ctrl" }, "m", function()
 -- 	hs.notify.new({ title = "Hammerspoon", informativeText = "Hello World" }):send()
 -- end)
+
+-- Main layout, browser on right most screen, Figma behind browser, Kitty on
+-- main screen. Email/Slack on left most screen.
+
+-- Split layout, browser on left of main screen, Kitty on right of main screen.
+-- Figma maximized on the right most screen, Email/Slack on left most screen.
