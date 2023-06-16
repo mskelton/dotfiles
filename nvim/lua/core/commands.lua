9816eda911123caefd9f883e7d94b90ebb343aff
Mark Skelton <mark@federato.ai>
2023-06-16 12:41:22 -0500

    JsonCopy

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -3,8 +3,13 @@ vim.api.nvim_create_user_command("Pomo", "silent !pomo <args>", { nargs = "*" })
 vim.api.nvim_create_user_command("Task", "!task <args>", { nargs = "*" })
 
 -- Format/unformat files with JQ
-vim.api.nvim_create_user_command("FormatJson", ":%!jq .", {})
-vim.api.nvim_create_user_command("UnformatJson", ":%!jq -c .", {})
+vim.api.nvim_create_user_command("JsonFormat", ":%!jq .", {})
+vim.api.nvim_create_user_command("JsonUnformat", ":%!jq -c .", {})
+vim.api.nvim_create_user_command(
+	"JsonCopy",
+	[[:silent %w !jq -c . | sed "s/\"/\\\\\"/g" | tr -d "\n" | pbcopy]],
+	{}
+)
 
 -- Create an ISC license in a project
 vim.api.nvim_create_user_command("CreateLicense", "!gh license isc", {})
