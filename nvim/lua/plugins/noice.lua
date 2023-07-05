98b5bfea42a653705878b2bb6d1acbdf5145ee8e
Mark Skelton <mark@federato.ai>
2023-07-05 09:02:23 -0500

    Disable basically everything in Noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,21 +3,21 @@ return {
 		"folke/noice.nvim",
 		dependencies = { "MunifTanjim/nui.nvim" },
 		lazy = false,
+		enabled = false,
 		config = function()
 			require("noice").setup({
-				cmdline = {
-					enabled = false,
-				},
-				messages = {
-					enabled = false,
-					view = "cmdline",
-				},
+				cmdline = { enabled = false },
+				messages = { enabled = false },
+				notify = { enabled = false },
 				lsp = {
 					progress = {
 						enabled = false,
 					},
 					hover = {
 						enabled = true,
+					},
+					message = {
+						enabled = false,
 					},
 					override = {
 						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
