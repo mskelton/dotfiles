feed01835ca46d3ba2bcffabc6e7432f31cfde81
Mark Skelton <mdskelton99@gmail.com>
2023-07-10 19:54:05 -0500

    Add dtsfmt to null-ls

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,4 +1,6 @@
 local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
+local methods = require("null-ls.methods")
 
 null_ls.setup({
 	sources = {
@@ -32,5 +34,21 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.fish_indent,
 		null_ls.builtins.formatting.rustfmt,
+		helpers.make_builtin({
+			name = "dtsfmt",
+			meta = {
+				url = "https://github.com/mskelton/dtsfmt",
+				description = "Auto formatter for device tree files",
+				notes = {},
+			},
+			method = methods.internal.FORMATTING,
+			filetypes = { "dts" },
+			generator_opts = {
+				command = "dtsfmt",
+				args = { "--emit=stdout" },
+				to_stdin = true,
+			},
+			factory = helpers.formatter_factory,
+		}),
 	},
 })
