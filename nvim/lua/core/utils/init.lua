031b4799205e560da7b783543d6c892800992e2f
Mark Skelton <mdskelton99@gmail.com>
2023-07-16 08:30:46 -0500

    Use single quotes for URL

--- nvim/lua/core/utils/init.lua
+++ nvim/lua/core/utils/init.lua
@@ -73,8 +73,8 @@ end
 --- Opens any URLs in the provided text string
 --- @param text string
 M.open_url = function(text)
-	local url = string.gsub(text, '"', '\\"')
-	os.execute('echo "' .. url .. '" | url | xargs open')
+	local url = string.gsub(text, "'", "\\'")
+	os.execute("echo '" .. url .. "' | url | xargs open")
 end
 
 --- Get's the truncation width for the current window size.
