49966c3f5bd8927b1c78866ccaeaeb80f3adf5f8
Mark Skelton <mdskelton99@gmail.com>
2023-07-17 17:09:24 -0500

    Add more command

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -1,25 +1,31 @@
--- Wrappers around some common CLI commands
-vim.api.nvim_create_user_command("Pomo", "silent !pomo <args>", { nargs = "*" })
-vim.api.nvim_create_user_command("Task", "!task <args>", { nargs = "*" })
-
--- GitHub CLI
+-- Run GitHub CLI (gh) command
 vim.api.nvim_create_user_command("H", "!gh <args>", { nargs = "*" })
 
--- Format/unformat files with JQ
-vim.api.nvim_create_user_command("JsonFormat", ":%!jq .", {})
-vim.api.nvim_create_user_command("JsonUnformat", ":%!jq -c .", {})
+-- Format JSON with jq
+vim.api.nvim_create_user_command("JsonFormat", ":!jq .", {})
+
+-- Minify JSON with jq
+vim.api.nvim_create_user_command("JsonMinify", ":!jq -c .", {})
+
+-- Copy stringified JSON to clipboard
 vim.api.nvim_create_user_command(
 	"JsonCopy",
-	[[:silent %w !jq -c . | sed "s/\"/\\\\\"/g" | tr -d "\n" | pbcopy]],
+	[[:silentw !jq -c . | sed "s/\"/\\\\\"/g" | tr -d "\n" | pbcopy]],
 	{}
 )
 
 -- Create an ISC license in a project
-vim.api.nvim_create_user_command("CreateLicense", "!gh license isc", {})
+vim.api.nvim_create_user_command("License", "!gh license isc", {})
 
--- Browse a URL in the default browser. Needed for vim-fugitive
+-- Search/replace type commands
+vim.api.nvim_create_user_command("RemoveEmptyLines", ":g/^$/d", { bar = true })
 vim.api.nvim_create_user_command(
-	"Browse",
-	"silent !open <args>",
-	{ nargs = "*" }
+	"RemoveTrailingWhitespace",
+	[[:%s/\s\+$//e]],
+	{ bar = true }
+)
+vim.api.nvim_create_user_command(
+	"UniqueLines",
+	":RemoveEmptyLines | sort u",
+	{ bar = true }
 )
