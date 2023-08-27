b1f5d4efeb7cd9a125f30b47ebce5150d93171ba
Mark Skelton <mdskelton99@gmail.com>
2023-08-26 23:49:57 -0500

    Don't use dev termicons

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,6 +1,11 @@
 return {
 	"nvim-tree/nvim-web-devicons",
-	dependencies = { "mskelton/termicons.nvim" },
+	dependencies = {
+		{
+			"mskelton/termicons.nvim",
+			dev = false,
+		},
+	},
 	config = function()
 		require("termicons").setup()
 	end,
