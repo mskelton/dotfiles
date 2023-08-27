67d74559d82db4d950581aa4f12e2bac128b2557
Mark Skelton <mdskelton99@gmail.com>
2023-08-27 08:50:21 -0500

    Move meeting windows to the right place

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -57,16 +57,50 @@ end
 --- Get's the active Around meeting window
 local function get_around_window(app_name)
 	local app = hs.application.get(app_name)
-	local windows = app:allWindows()
+	if app == nil then
+		return {}
+	end
 
 	-- For some reason, when there is only one window, Around is the name of the
 	-- lobby, but when in a meeting, it's the name of the meeting. I only ever
 	-- want to move the meeting window.
-	if #windows == 2 then
+	if #app:allWindows() == 2 then
 		return { app:getWindow("Around") }
 	end
 
 	return {}
+end
+
+--- Puts a window on the left side of the screen without resizing it
+--- @param window hs.window
+local function put_left(window)
+	local screen = window:screen():frame()
+	local frame = window:frame()
+
+	return hs.geometry
+		.rect(
+			screen.x + (screen.w / 4) - (frame.w / 2),
+			screen.y + (screen.h / 2) - (frame.h / 2),
+			frame.w,
+			frame.h
+		)
+		:toUnitRect(screen)
+end
+
+--- Puts a window in the center side of the screen without resizing it
+--- @param window hs.window
+local function put_center(window)
+	local screen = window:screen():frame()
+	local frame = window:frame()
+
+	return hs.geometry
+		.rect(
+			screen.x + (screen.w / 2) - (frame.w / 2),
+			screen.y + (screen.h / 2) - (frame.h / 2),
+			frame.w,
+			frame.h
+		)
+		:toUnitRect(screen)
 end
 
 -- Main layout, browser on right most screen, Figma behind browser, kitty on
@@ -77,6 +111,7 @@ hs.hotkey.bind(layer_key, "u", function()
 		{ "kitty", nil, screens.laptop, hs.layout.maximized, nil, nil },
 		{ "Mimestream", nil, screens.laptop, hs.layout.maximized, nil, nil },
 		{ "Slack", nil, screens.laptop, hs.layout.maximized, nil, nil },
+		{ "zoom.us", "Zoom Meeting", screens.laptop, hs.layout.maximized, nil, nil },
 	}, {}, {
 		-- Primary
 		{ "kitty", nil, screens.primary, hs.layout.maximized, nil, nil },
@@ -95,10 +130,12 @@ end)
 -- Figma maximized on the right most screen, Email/Slack on left most screen.
 hs.hotkey.bind(layer_key, "i", function()
 	apply_layout({
-		{ "Arc", nil, screens.laptop, hs.layout.left50, nil, nil },
+		{ "Arc", nil, screens.laptop, hs.layout.right50, nil, nil },
 		{ "kitty", nil, screens.laptop, hs.layout.right50, nil, nil },
-		{ "zoom.us", "Zoom Meeting", screens.laptop, hs.layout.maximized, nil, nil },
-		{ "Around", get_around_window, screens.laptop, hs.layout.maximized, nil, nil },
+		{ "zoom.us", "Zoom Meeting", screens.laptop, hs.layout.left50, nil, nil },
+		{ "zoom.us", "Zoom", screens.laptop, put_left, nil, nil },
+		{ "Around", get_around_window, screens.laptop, hs.layout.left50, nil, nil },
+		{ "Around", "Around", screens.laptop, put_left, nil, nil },
 		{ "Mimestream", nil, screens.laptop, hs.layout.maximized, nil, nil },
 		{ "Slack", nil, screens.laptop, hs.layout.maximized, nil, nil },
 	}, {}, {
@@ -108,7 +145,9 @@ hs.hotkey.bind(layer_key, "i", function()
 		-- Secondary
 		{ "Figma", nil, screens.secondary, hs.layout.maximized, nil, nil },
 		{ "zoom.us", "Zoom Meeting", screens.secondary, hs.layout.maximized, nil, nil },
+		{ "zoom.us", "Zoom", screens.laptop, put_center, nil, nil },
 		{ "Around", get_around_window, screens.secondary, hs.layout.maximized, nil, nil },
+		{ "Around", "Around", screens.laptop, put_center, nil, nil },
 		-- Laptop
 		{ "Mimestream", nil, screens.laptop, hs.layout.maximized, nil, nil },
 		{ "Slack", nil, screens.laptop, hs.layout.maximized, nil, nil },
@@ -120,9 +159,12 @@ end)
 -- most screen.
 hs.hotkey.bind(layer_key, "o", function()
 	apply_layout({
-		{ "Arc", nil, screens.laptop, hs.layout.left50, nil, nil },
-		{ "kitty", nil, screens.laptop, hs.layout.left50, nil, nil },
-		{ "zoom.us", "Zoom Meeting", screens.laptop, hs.layout.right50, nil, nil },
+		{ "Arc", nil, screens.laptop, hs.layout.right50, nil, nil },
+		{ "kitty", nil, screens.laptop, hs.layout.right50, nil, nil },
+		{ "zoom.us", "Zoom Meeting", screens.laptop, hs.layout.left50, nil, nil },
+		{ "Zoom", "Zoom", screens.primary, put_left, nil, nil },
+		{ "Around", get_around_window, screens.primary, hs.layout.left50, nil, nil },
+		{ "Around", "Around", screens.primary, put_left, nil, nil },
 		{ "Mimestream", nil, screens.laptop, hs.layout.maximized, nil, nil },
 		{ "Slack", nil, screens.laptop, hs.layout.maximized, nil, nil },
 	}, {}, {
@@ -130,7 +172,9 @@ hs.hotkey.bind(layer_key, "o", function()
 		{ "Arc", nil, screens.primary, hs.layout.right50, nil, nil },
 		{ "kitty", nil, screens.primary, hs.layout.right50, nil, nil },
 		{ "zoom.us", "Zoom Meeting", screens.primary, hs.layout.left50, nil, nil },
+		{ "Zoom", "Zoom", screens.primary, put_left, nil, nil },
 		{ "Around", get_around_window, screens.primary, hs.layout.left50, nil, nil },
+		{ "Around", "Around", screens.primary, put_left, nil, nil },
 		-- Secondary
 		{ "Figma", nil, screens.secondary, hs.layout.maximized, nil, nil },
 		-- Laptop
@@ -142,4 +186,13 @@ end)
 -- Escape paste-blocking
 hs.hotkey.bind({ "cmd", "alt" }, "v", function()
 	hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+end)
+
+-- Audio presets
+hs.hotkey.bind({ "cmd", "ctrl" }, "f9", function()
+	hs.audiodevice.defaultOutputDevice():setOutputVolume(40)
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "f10", function()
+	hs.audiodevice.defaultOutputDevice():setOutputVolume(80)
 end)
