6c62ef3ae96585fd5554fa49f9c81c5805c2595e
Mark Skelton <mdskelton99@gmail.com>
2023-08-30 07:43:15 -0500

    More git shortcuts

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,5 @@
+local nv = { "n", "v" }
+
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "BufReadPre",
@@ -7,15 +9,29 @@ return {
 			"<leader>vs",
 			":Gitsigns stage_hunk<cr>",
 			silent = true,
-			mode = "v",
+			mode = nv,
 			desc = "Git: stage hunk",
 		},
 		{
 			"<leader>vu",
 			":Gitsigns reset_hunk<cr>",
 			silent = true,
-			mode = "v",
-			desc = "Git: reset hunk",
+			mode = nv,
+			desc = "Git: unstage hunk",
+		},
+		{
+			"[v",
+			"<cmd>Gitsigns prev_hunk<cr>",
+			silent = true,
+			mode = nv,
+			desc = "Git: previous hunk",
+		},
+		{
+			"]v",
+			"<cmd>Gitsigns next_hunk<cr>",
+			silent = true,
+			mode = nv,
+			desc = "Git: next hunk",
 		},
 	},
 }
