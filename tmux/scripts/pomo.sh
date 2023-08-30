fc7715971cb206827a6ae4a7054e57aa582501ab
Mark Skelton <mdskelton99@gmail.com>
2023-08-29 23:18:33 -0500

    Move pomo status to left

--- tmux/scripts/pomo.sh
+++ tmux/scripts/pomo.sh
@@ -5,5 +5,5 @@ status=$(pomo)
 # Prints the pomodoro status with a more obvious background when in tmux. If
 # there is no active session, this will not print anything.
 if [[ ! -z "$status" ]]; then
-	echo "#[fg=#3b4261]#[fg=#7aa2f7,bg=#3b4261] $status "
+	echo "#[fg=#737aa2]$status"
 fi
