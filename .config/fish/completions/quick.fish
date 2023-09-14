9830e59ce65f91521fdeea0fd74b819ccf012e9a
Mark Skelton <mark@federato.ai>
2023-09-14 15:44:39 -0500

    Update completions

--- .config/fish/completions/quick.fish
+++ .config/fish/completions/quick.fish
@@ -1,4 +1,4 @@
-complete -c quick -f -n __fish_use_subcommand -a list -d 'List open issues'
-complete -c quick -f -n __fish_use_subcommand -a add -d 'Add a new issue'
-complete -c quick -f -n __fish_use_subcommand -a close -d 'Close one or more issues'
+complete -c quick -f -n __fish_use_subcommand -a list -d 'List tasks'
+complete -c quick -f -n __fish_use_subcommand -a add -d 'Add a task'
+complete -c quick -f -n __fish_use_subcommand -a close -d 'Close tasks'
 complete -c quick -f -n __fish_use_subcommand -a help -d 'Print help'
