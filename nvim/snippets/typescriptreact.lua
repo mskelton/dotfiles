5832c528c33806213b740416810297f7d942906b
Mark Skelton <mark@federato.ai>
2023-11-03 11:52:38 -0500

    Render props

--- nvim/snippets/typescriptreact.lua
+++ nvim/snippets/typescriptreact.lua
@@ -3,6 +3,7 @@ local snip_utils = require("utils.snip_utils")
 
 return {
 	parse("chil", "{ children }: { children: React.ReactNode }"),
+	parse("rrp", "{({ $1 }) => (\n\t<>\n\t\t$0\n\t</>\n)}"),
 	-- React components
 	s(
 		"rd",
