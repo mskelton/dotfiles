6c113c55ffe56d59381262138ff6401e187a879f
Mark Skelton <mdskelton99@gmail.com>
2023-11-08 21:57:31 -0600

    Octo

---
+++ nvim/lua/plugins/octo.lua
@@ -0,0 +1,10 @@
+return {
+	"pwntester/octo.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	cmd = { "Octo" },
+	config = true,
+}
