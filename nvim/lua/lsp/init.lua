edc2a231ca57578eedae111e8a5e0e7ca11c615d
Mark Skelton <mdskelton99@gmail.com>
2023-11-22 08:20:17 -0600

    Setup efm

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -115,6 +115,7 @@ M.setup_servers = function()
 
 	M.server("eslint")
 	M.server("cssls")
+	M.server("efm", require("lsp.efm").config())
 
 	-- Only enable Tailwind if the project has a Tailwind config file
 	M.server("tailwindcss", {
@@ -158,7 +159,6 @@ M.setup = function()
 
 	require("lsp.handlers").register_handlers()
 	require("lsp.autocmd")
-	require("lsp.null-ls")
 	require("neodev").setup({})
 
 	M.setup_servers()
