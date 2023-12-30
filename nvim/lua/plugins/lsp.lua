9d7b7537286dfb97b87e0aabac7a54192e0b3d63
Mark Skelton <mdskelton99@gmail.com>
2023-12-30 14:15:34 -0600

    Disable efm configs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
 		"mskelton/null-ls.nvim",
 		"ray-x/lsp_signature.nvim",
 		"onsails/lspkind-nvim",
-		"creativenull/efmls-configs-nvim",
+		--- "creativenull/efmls-configs-nvim",
 	},
 	config = function()
 		return require("lsp").setup()
