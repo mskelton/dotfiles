124de33d5da8610689e9c4eb5c7a5af725b006b0
Mark Skelton <mdskelton99@gmail.com>
2024-02-02 23:45:49 -0600

    Update ecma highlights

--- nvim/after/queries/devicetree/highlights.scm
+++ nvim/after/queries/devicetree/highlights.scm
@@ -1,13 +1,12 @@
 ; extends
 
-; TODO: Enable these once https://github.com/joelspadin/tree-sitter-devicetree/pull/12 is merged
-; [
-;   "#if"
-;   "#ifdef"
-;   "#ifndef"
-;   "#else"
-;   "#elif"
-;   "#endif"
-;   "#elifdef"
-;   "#elifndef"
-; ] @preproc
+[
+  "#if"
+  "#ifdef"
+  "#ifndef"
+  "#else"
+  "#elif"
+  "#endif"
+  "#elifdef"
+  "#elifndef"
+] @preproc
