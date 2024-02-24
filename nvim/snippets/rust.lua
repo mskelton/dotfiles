cb706b9e6291f1eeb995860b6cb17c497c2cb24a
Mark Skelton <mdskelton99@gmail.com>
2024-02-24 11:34:02 -0600

    Rust test functino

--- nvim/snippets/rust.lua
+++ nvim/snippets/rust.lua
@@ -1,6 +1,14 @@
 --- @diagnostic disable: undefined-global
 
+local test = [[
+#[test]
+fn $1() {
+    $0
+}
+]]
+
 return {
 	parse("cl", 'println!("{:?}", $0);'),
 	parse("fn", "fn $1($2) {\n\t$0\n}"),
+	parse("tit", test),
 }
