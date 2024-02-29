d084497d3f3d7987b24cf475864f33cdbac4d0e9
Mark Skelton <mdskelton99@gmail.com>
2024-02-29 13:13:17 -0600

    Events

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		event = "BufReadPost",
+		event = { "BufReadPre", "BufNewFile" },
 		cmd = {
 			"TSBufDisable",
 			"TSBufEnable",
