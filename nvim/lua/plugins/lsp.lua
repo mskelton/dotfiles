c5babaf9b86e5d026607c4ed680dfc0c9e8452a2
Mark Skelton <mdskelton99@gmail.com>
2024-03-01 21:15:59 -0600

    Cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
 		"mskelton/null-ls.nvim",
 		"ray-x/lsp_signature.nvim",
 		"onsails/lspkind-nvim",
-		--- "creativenull/efmls-configs-nvim",
 	},
 	config = function()
 		return require("lsp").setup()
