57a43468deb558ed2994df2d4dc35a6dfee90c6e
Mark Skelton <mdskelton99@gmail.com>
2024-03-21 19:52:29 -0500

    Paste markdown image

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -5,3 +5,7 @@ vim.opt_local.spelllang = "en_us"
 --- Hard wrapping in markdown
 vim.opt_local.textwidth = 80
 vim.opt_local.formatoptions:append("t")
+
+vim.keymap.set("n", "<leader>pi", "<cmd>read !img --format md<cr>", {
+	desc = "Paste image",
+})
