3e694fb965ccd7abef41bf1f360aebbc4ca55714
Mark Skelton <mdskelton99@gmail.com>
2024-03-23 11:22:17 -0500

    Improve Prettier config detection

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,8 +1,40 @@
 local null_ls = require("null-ls")
+local utils = require("null-ls.utils")
+
+--- Check if the project has a package manifest configuration
+--- @param conditions table
+--- @param key string
+local function has_manifest_config(conditions, key)
+	if conditions.root_has_file({ "package.json" }) then
+		local filename = utils.get_root() .. "/package.json"
+		local manifest = vim.json.decode(table.concat(vim.fn.readfile(filename)))
+
+		--- Check if the manifest has the key
+		return manifest ~= nil and manifest[key] ~= nil
+	end
+
+	return false
+end
 
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.prettierd.with({
+			condition = function(conditions)
+				return conditions.root_has_file({
+					".prettierrc",
+					".prettierrc.cjs",
+					".prettierrc.js",
+					".prettierrc.json",
+					".prettierrc.json5",
+					".prettierrc.mjs",
+					".prettierrc.toml",
+					".prettierrc.yaml",
+					".prettierrc.yml",
+					"prettier.config.cjs",
+					"prettier.config.js",
+					"prettier.config.mjs",
+				}) or has_manifest_config(conditions, "prettier")
+			end,
 			env = {
 				PRETTIERD_LOCAL_PRETTIER_ONLY = true,
 			},
