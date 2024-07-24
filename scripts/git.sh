816a2867dfd6a5aaebe0c6ea5e791488bf5fe661
Mark Skelton <mdskelton99@gmail.com>
2024-07-24 08:17:20 -0500

    Update git setup

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 user=$(whoami)
-read -p 'What is your email? ' email
 
 # Create the ssh key
 mkdir -p "$HOME/.ssh"
 
 # Create the default global gitconfig
 cat <<EOF >"$HOME/.gitconfig"
-[user]
-	name = Mark Skelton
-	email = $email
 [core]
 	excludesfile = /Users/$user/.gitignore-global
 [include]
