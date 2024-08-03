69deb59169749d2581418b0205ef1af64addd6e3
Mark Skelton <mdskelton99@gmail.com>
2024-08-03 12:05:57 -0500

    Update git signing keys

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,10 +5,22 @@ user=$(whoami)
 # Create the ssh key
 mkdir -p "$HOME/.ssh"
 
+read -rp "Enter your personal email: " personal_email
+read -rp "Enter your work email: " work_email
+
+if [[ -f "$HOME/.work" ]]; then
+	keyfile=id_ed25519_personal_sk.pub
+	ssh-keygen -t ed25519 -f "$HOME/.ssh/$keyfile" -C "$personal_email"
+	ssh-keygen -t ed25519 -f "$HOME/.ssh/id_ed25519_sk_work.pub" -C "$work_email"
+else
+	keyfile=id_ed25519_sk
+	ssh-keygen -t ed25519 -f "$HOME/.ssh/$keyfile" -C "$work_email"
+fi
+
 # Create the default global gitconfig
 cat <<EOF >"$HOME/.gitconfig"
 [user]
-  signingkey = /Users/$user/.ssh/id_ed25519_personal.pub
+  signingkey = /Users/$user/.ssh/$keyfile
 [core]
 	excludesfile = /Users/$user/.gitignore-global
 [include]
