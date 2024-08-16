cfa802f0c5f0557addd6b05fa3e928369949838a
Mark Skelton <mdskelton99@gmail.com>
2024-08-16 08:20:48 -0500

    Fix null-ls formatting in node_modules

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,5 +1,6 @@
 local null_ls = require("null-ls")
 local utils = require("null-ls.utils")
+local helpers = require("null-ls.helpers")
 
 --- Check if the project has a package manifest configuration
 --- @param conditions table
@@ -16,25 +17,44 @@ local function has_manifest_config(conditions, key)
 	return false
 end
 
+local prettier_config_files = {
+	".prettierrc",
+	".prettierrc.cjs",
+	".prettierrc.js",
+	".prettierrc.json",
+	".prettierrc.json5",
+	".prettierrc.mjs",
+	".prettierrc.toml",
+	".prettierrc.yaml",
+	".prettierrc.yml",
+	"prettier.config.cjs",
+	"prettier.config.js",
+	"prettier.config.mjs",
+}
+
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.prettierd.with({
 			condition = function(conditions)
-				return conditions.root_has_file({
-					".prettierrc",
-					".prettierrc.cjs",
-					".prettierrc.js",
-					".prettierrc.json",
-					".prettierrc.json5",
-					".prettierrc.mjs",
-					".prettierrc.toml",
-					".prettierrc.yaml",
-					".prettierrc.yml",
-					"prettier.config.cjs",
-					"prettier.config.js",
-					"prettier.config.mjs",
-				}) or has_manifest_config(conditions, "prettier")
+				return conditions.root_has_file(prettier_config_files)
+					or has_manifest_config(conditions, "prettier")
 			end,
+			cwd = helpers.cache.by_bufnr(function(params)
+				--- Small hack to prevent unwanted formatting of files in node_modules.
+				--- If a file is in node_modules, it will likely find a `package.json`
+				--- file as an ancestor, and then use that as the cwd, which will cause
+				--- `.prettierignore` files in the root of the project to not be
+				--- respected. To fix this, we strip out the `node_modules` part of the
+				--- path so that we always resolve the cwd to actual project files. This
+				--- still might not be the root of the project in the case of a monorepo,
+				--- but it prevents any directory in `node_modules` from being used as
+				--- the cwd.
+				local bufname = string.gsub(params.bufname, "node_modules/.*", "")
+
+				return utils.root_pattern(prettier_config_files, "package.json")(
+					bufname
+				)
+			end),
 			env = {
 				PRETTIERD_LOCAL_PRETTIER_ONLY = true,
 			},
