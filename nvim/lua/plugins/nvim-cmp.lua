e2c62a57fdbdb0aca30292a3a4bd9cc80b04d9e5
Mark Skelton <mdskelton99@gmail.com>
2024-08-23 13:27:51 -0500

    Sort snippets first

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -167,6 +167,32 @@ return {
 					return kind
 				end,
 			},
+			sorting = {
+				comparators = {
+					--- Sort snippets first
+					function(a, b)
+						local a_kind = a:get_kind()
+						local b_kind = b:get_kind()
+						local snip = cmp.lsp.CompletionItemKind.Snippet
+
+						if a_kind == snip and b_kind ~= snip then
+							return true
+						elseif b_kind == snip and a_kind ~= snip then
+							return false
+						end
+					end,
+					cmp.config.compare.offset,
+					cmp.config.compare.exact,
+					--- cmp.config.compare.scopes,
+					cmp.config.compare.score,
+					cmp.config.compare.recently_used,
+					cmp.config.compare.locality,
+					cmp.config.compare.kind,
+					--- cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
+				},
+			},
 		})
 
 		local cmdline_next = {
