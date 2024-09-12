039417a763b79bf037b0964773825d36094aad30
Mark Skelton <mdskelton99@gmail.com>
2024-09-12 16:22:23 -0500

    Fix warnings

--- nvim/lua/lsp/autocmd.lua
+++ nvim/lua/lsp/autocmd.lua
@@ -44,6 +44,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		local opts = { buffer = bufnr, silent = true }
 
+		--- Shouldn't happen, but just in case
+		if client == nil then
+			return
+		end
+
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
@@ -118,7 +123,7 @@ local function code_actions_on_write(pattern, code_actions)
 end
 
 on_write("*", function(opts)
-	local clients = vim.lsp.get_active_clients({ bufnr = opts.buf })
+	local clients = vim.lsp.get_clients({ bufnr = opts.buf })
 
 	--- Only run formatting if there are connected LSP clients
 	if vim.tbl_count(clients) ~= 0 then
