cf2732afc3fde0a99c8990721cfc7dcdbbd17dd0
Mark Skelton <mdskelton99@gmail.com>
2024-10-11 12:14:40 -0500

    Improve path

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,19 +1,10 @@
-# Custom binaries
 set -g fish_user_paths $fish_user_paths "$HOME/.local/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/.local/bin-work"
-
-# Misc
 set -g fish_user_paths $fish_user_paths "$HOME/.cargo/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/.local/share/npm/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/.bun/bin"
-set -g fish_user_paths $fish_user_paths "$GOPATH/bin"
-set -g fish_user_paths $fish_user_paths "$fzf_base/bin"
-set -g fish_user_paths $fish_user_paths "$PYENV_ROOT/bin"
+set -g fish_user_paths $fish_user_paths "$HOME/go/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/flutter/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/Library/Application Support/fnm"
 set -g fish_user_paths $fish_user_paths "$HOME/.slack/bin"
-
-# Android studio
-set -g fish_user_paths $fish_user_paths "$ANDROID_HOME/tools"
-set -g fish_user_paths $fish_user_paths "$ANDROID_HOME/tools/bin"
-set -g fish_user_paths $fish_user_paths "$ANDROID_HOME/platform-tools"
+set -g fish_user_paths $fish_user_paths /opt/homebrew/bin
