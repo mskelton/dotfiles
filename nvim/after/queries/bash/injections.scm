91a53dc82b48355ea1e99b41ef24e987d3307706
Mark Skelton <mdskelton99@gmail.com>
2024-11-07 08:18:00 -0600

    Cleanup

--- nvim/after/queries/bash/injections.scm
+++ nvim/after/queries/bash/injections.scm
@@ -4,7 +4,7 @@
 (command
   name: ((command_name) @_name
     (#eq? @_name "awk"))
-  argument: ((raw_string) @awk @injection.content)
+  argument: ((raw_string) @injection.content)
     (#set! injection.include-children)
     (#set! injection.language "awk")
     (#offset! @injection.content 0 1 0 -1))
