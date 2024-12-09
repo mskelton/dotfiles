8c05f3321f0a497c174c7a95e623cc1ee975463c
Mark Skelton <mdskelton99@gmail.com>
2024-12-08 22:27:53 -0600

    GritQL highlights

---
+++ nvim/queries/gritql/highlights.scm
@@ -0,0 +1,90 @@
+(comment) @comment
+(stringConstant) @string
+(intConstant) @number
+(signedIntConstant) @number
+(doubleConstant) @float
+(booleanConstant) @boolean
+(undefined) @keyword
+(top) @keyword
+(bottom) @keyword
+(variable) @variable
+(name) @identifier
+(annotation) @annotation
+(regex) @string.regex
+(snippetRegex) @string.special
+(codeSnippet) @string.special
+(language_flavor) @constant
+(foreignLanguageName) @constant
+(languageName) @constant
+
+(patternDefinition) @function.definition
+(predicateDefinition) @function.definition
+(functionDefinition) @function.definition
+(foreignFunctionDefinition) @function.definition
+
+(predicateCall) @function
+(log) @function
+(range) @function
+
+(rewrite) @operator
+(patternAs) @operator
+(patternLimit) @operator
+(assignmentAsPattern) @operator
+(patternAccumulate) @operator
+(patternWhere) @operator
+(mulOperation) @operator
+(divOperation) @operator
+(modOperation) @operator
+(addOperation) @operator
+(subOperation) @operator
+(predicateRewrite) @operator
+(predicateAssignment) @operator
+(predicateAccumulate) @operator
+(predicateGreater) @operator
+(predicateLess) @operator
+(predicateGreaterEqual) @operator
+(predicateLessEqual) @operator
+(predicateNotEqual) @operator
+(predicateEqual) @operator
+(predicateMatch) @operator
+(listIndex) @operator
+(dot) @punctuation.delimiter
+
+(map) @constructor
+(mapElement) @field
+(mapAccessor) @field
+(list) @constructor
+
+(patternIfElse) @conditional
+(patternMaybe) @conditional
+(patternAnd) @conditional
+(patternOr) @conditional
+(patternOrElse) @conditional
+(patternAny) @conditional
+(patternNot) @conditional
+(predicateIfElse) @conditional
+(predicateMaybe) @conditional
+(predicateOr) @conditional
+(predicateAnd) @conditional
+(predicateAny) @conditional
+(predicateNot) @conditional
+
+(patternIncludes) @keyword
+(patternContains) @keyword
+(patternBefore) @keyword
+(patternAfter) @keyword
+(within) @keyword
+(like) @keyword
+(some) @keyword
+(every) @keyword
+(bubble) @keyword
+(sequential) @keyword
+(files) @keyword
+
+(patternDefinitionBody) @block
+(predicateDefinitionBody) @block
+(functionDefinitionBody) @block
+(foreignFunctionBody) @block
+
+(version) @keyword
+(langdecl) @keyword
