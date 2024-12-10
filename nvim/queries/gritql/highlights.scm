45a50dfa9e91b193346b1e0e93123711e95bcc2b
Mark Skelton <mdskelton99@gmail.com>
2024-12-09 23:14:34 -0600

    GritQL

--- nvim/queries/gritql/highlights.scm
+++ nvim/queries/gritql/highlights.scm
@@ -1,90 +1,29 @@
 (comment) @comment
-(stringConstant) @string
-(intConstant) @number
-(signedIntConstant) @number
-(doubleConstant) @float
-(booleanConstant) @boolean
-(undefined) @keyword
-(top) @keyword
-(bottom) @keyword
-(variable) @variable
-(name) @identifier
-(annotation) @annotation
-(regex) @string.regex
-(snippetRegex) @string.special
-(codeSnippet) @string.special
-(language_flavor) @constant
-(foreignLanguageName) @constant
-(languageName) @constant
 
-(patternDefinition) @function.definition
-(predicateDefinition) @function.definition
-(functionDefinition) @function.definition
-(foreignFunctionDefinition) @function.definition
+[
+  "."
+] @punctuation.delimiter
 
-(predicateCall) @function
-(log) @function
-(range) @function
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
 
-(rewrite) @operator
-(patternAs) @operator
-(patternLimit) @operator
-(assignmentAsPattern) @operator
-(patternAccumulate) @operator
-(patternWhere) @operator
-(mulOperation) @operator
-(divOperation) @operator
-(modOperation) @operator
-(addOperation) @operator
-(subOperation) @operator
-(predicateRewrite) @operator
-(predicateAssignment) @operator
-(predicateAccumulate) @operator
-(predicateGreater) @operator
-(predicateLess) @operator
-(predicateGreaterEqual) @operator
-(predicateLessEqual) @operator
-(predicateNotEqual) @operator
-(predicateEqual) @operator
-(predicateMatch) @operator
-(listIndex) @operator
-(dot) @punctuation.delimiter
+[
+ "where"
+ "limit"
+ "sequential"
+ "includes"
+ "or"
+] @keyword
 
-(map) @constructor
-(mapElement) @field
-(mapAccessor) @field
-(list) @constructor
+[
+  "=>"
+  "<:"
+] @operator
 
-(patternIfElse) @conditional
-(patternMaybe) @conditional
-(patternAnd) @conditional
-(patternOr) @conditional
-(patternOrElse) @conditional
-(patternAny) @conditional
-(patternNot) @conditional
-(predicateIfElse) @conditional
-(predicateMaybe) @conditional
-(predicateOr) @conditional
-(predicateAnd) @conditional
-(predicateAny) @conditional
-(predicateNot) @conditional
-
-(patternIncludes) @keyword
-(patternContains) @keyword
-(patternBefore) @keyword
-(patternAfter) @keyword
-(within) @keyword
-(like) @keyword
-(some) @keyword
-(every) @keyword
-(bubble) @keyword
-(sequential) @keyword
-(files) @keyword
-
-(patternDefinitionBody) @block
-(predicateDefinitionBody) @block
-(functionDefinitionBody) @block
-(foreignFunctionBody) @block
-
-(version) @keyword
-(langdecl) @keyword
+(backtickSnippet) @string
