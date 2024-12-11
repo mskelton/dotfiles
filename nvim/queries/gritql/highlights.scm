1f1bcb60cf9c76697f29e2199caf78ae86ac4c58
Mark Skelton <mdskelton99@gmail.com>
2024-12-10 18:02:34 -0600

    GritQL highlights

--- nvim/queries/gritql/highlights.scm
+++ nvim/queries/gritql/highlights.scm
@@ -1,9 +1,3 @@
-(comment) @comment
-
-[
-  "."
-] @punctuation.delimiter
-
 [
   "("
   ")"
@@ -26,4 +20,100 @@
   "<:"
 ] @operator
 
-(backtickSnippet) @string
+[
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "="
+  "=>"
+  "+="
+  ">"
+  "<"
+  ">="
+  "<="
+  "!="
+  "=="
+  "<:"
+] @operator
+
+[
+ "."
+ ","
+ ":"
+ ";"
+] @punctuation.delimiter
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+ "after"
+ "and"
+ "any"
+ "as"
+ "before"
+ "bubble"
+ "contains"
+ "else"
+ "every"
+ "if"
+ "includes"
+ "like"
+ "limit"
+ "maybe"
+ "multifile"
+ "not"
+ "or"
+ "orelse"
+ "sequential"
+ "some"
+ "where"
+ "within"
+] @keyword
+
+; Keywords
+(undefined) @keyword
+(top) @keyword
+(bottom) @keyword
+(version) @keyword
+
+; Comments
+(comment) @comment
+
+; Language
+(langdecl) @keyword
+(langdecl (languageName) @constant)
+(langdecl (language_flavor) @constant)
+
+; Strings
+(codeSnippet (backtickSnippet) @string)
+(stringConstant) @string
+(regex) @string.regex
+(snippetRegex) @string.special
+(codeSnippet) @string.special
+
+; Numbers
+(intConstant) @number
+(signedIntConstant) @number
+(doubleConstant) @number
+
+; Boolean
+(booleanConstant) @boolean
+
+; Functions
+(patternDefinition) @function.definition
+(predicateDefinition) @function.definition
+(functionDefinition) @function.definition
+(foreignFunctionDefinition) @function.definition
+
+; Variables
+(variable) @variable
+(annotation) @variable
