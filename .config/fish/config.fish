73e26ef167376fee4857a0d0bdf7ac5673b05ef0
Mark Skelton <mdskelton99@gmail.com>
2024-12-13 12:32:45 -0600

    Fix fnm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,7 +25,7 @@ source $HOME/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
 
 # Setup fnm
 if type -q fnm
-    fnm env | source
+    fnm env --use-on-cd --corepack-enabled --log-level error --shell fish | source
 end
 
 # Setup Zoxide
