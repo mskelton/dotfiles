20b19b643d2258f5c370c0bc037035682ba4f22b
Mark Skelton <mdskelton99@gmail.com>
2025-01-10 08:16:15 -0600

    Sync

--- .config/kitty/tmux_session.py
+++ .config/kitty/tmux_session.py
@@ -14,22 +14,22 @@ def handle_result(args: list[str], answer: str, target_window_id: int, boss: Bos
     sessions = sorted(sessions, key=lambda x: int(x.split(' ')[0]))
     sessions = [x.split(' ')[1] for x in sessions]
 
+    if len(sessions) == 0:
+        return
+
     # Get current session
     current_session = tmux(["display-message", "-p", "#{session_name}"])[0]
-
-    # Get the total number of sessions
-    total_sessions=len(sessions)
 
     # Get the current session index
     current_index = sessions.index(current_session)
 
     # Get the next session index based on the direction, loop around if necessary
-    next_index = (current_index + inc) % total_sessions
+    next_index = (current_index + inc) % len(sessions)
 
     # Switch to the requested session
     tmux(["switch-client", "-t", sessions[next_index]])
 
 def tmux(args: list[str]) -> list[str]:
     cmd = ["/opt/homebrew/bin/tmux"] + args
-    return subprocess.check_output(cmd, text=True).strip().split("\n")
-
+    output = subprocess.check_output(cmd, text=True).strip()
+    return output.splitlines() if output else []
