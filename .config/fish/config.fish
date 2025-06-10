3fbae8fcf330622b17a704c8ed403514742e20fc
Mark Skelton <mdskelton99@gmail.com>
2025-06-10 09:22:09 -0400

    More allowed tools for CC

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,3 +36,7 @@ if type -q direnv
 end
 
 source $HOME/.config/fish/path.fish
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
