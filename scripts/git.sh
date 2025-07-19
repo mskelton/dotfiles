93e655aba145b1ac0c2e53ae6345dfbc4972e2c8
Mark Skelton <mdskelton99@gmail.com>
2025-07-19 00:13:49 -0500

    Update GIt script

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,30 +1,50 @@
 #!/usr/bin/env bash
 
 user=$(whoami)
-
-# Create the ssh key
 mkdir -p "$HOME/.ssh"
 
-read -rp "Enter your personal email: " personal_email
-read -rp "Enter your work email: " work_email
+personal_email="mdskelton99@gmail.com"
+work_email="mskelton@ramp.com"
+
+home_keyfile="$HOME/.ssh/id_ed25519_home"
+work_keyfile="$HOME/.ssh/id_ed25519_sk_work"
+ssh-keygen -t ed25519 -N '' -f "$home_keyfile" -C "$personal_email"
 
 if [[ -f "$HOME/.work" ]]; then
-	keyfile=id_ed25519_sk_personal
-	ssh-keygen -t ed25519 -N '' -f "$HOME/.ssh/$keyfile" -C "$personal_email"
-	ssh-keygen -t ed25519 -N '' -f "$HOME/.ssh/id_ed25519_sk_work" -C "$work_email"
+	ssh-keygen -t ed25519 -N '' -f "$work_keyfile" -C "$work_email"
+
+	cat <<EOF >"$HOME/.ssh/config"
+Host *
+  IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+Host home personal
+  HostName github.com
+  User git
+  IdentityFile $home_keyfile
+  IdentitiesOnly yes
+
+Host github.com work
+  HostName github.com
+  User git
+  IdentityFile $work_keyfile
+  IdentitiesOnly yes
+EOF
 else
-	keyfile=id_ed25519_sk
-	ssh-keygen -t ed25519 -N '' -f "$HOME/.ssh/$keyfile" -C "$work_email"
+	cat <<EOF >"$HOME/.ssh/config"
+Host github.com
+  User git
+  IdentityFile $home_keyfile
+  IdentitiesOnly yes
+EOF
 fi
 
-# Create the default global gitconfig
 cat <<EOF >"$HOME/.gitconfig"
 [user]
-  signingkey = /Users/$user/.ssh/$keyfile.pub
+  signingkey = $home_keyfile.pub
 [core]
 	excludesfile = /Users/$user/.gitignore-global
 [include]
 	path = /Users/$user/.gitconfig-shared
-[includeIf "hasconfig:remote.*.url:work:ramp/**"]
+[includeIf "hasconfig:remote.*.url:*:ramp/**"]
 	path = /Users/$user/.gitconfig-work
 EOF
