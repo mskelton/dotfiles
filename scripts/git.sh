660f703e0f0d51518cd6380023614a00527bebda
Mark Skelton <mdskelton99@gmail.com>
2026-08-17 15:09:59 -0700

    Dual HTTPS/SSH Git config

--- scripts/git.sh
+++ scripts/git.sh
@@ -20,6 +20,19 @@ cat <<EOF >"$HOME/.gitconfig"
 	excludesfile = $HOME/.gitignore-global
 [include]
 	path = $HOME/.gitconfig-shared
-[includeIf "hasconfig:remote.*.url:*:anysphere/**"]
+[includeIf "hasconfig:remote.*.url:*:ramp/**"]
 	path = $HOME/.gitconfig-work
 EOF
+
+
+if [[ -f "$HOME/.work" ]]; then
+  read -rp "Work email: " work_email || work_email=""
+
+  ssh-keygen -t ed25519 -N '' -f "$HOME/.ssh/id_ed25519_sk_work" -C "$work_email"
+
+  cat <<EOF >"$HOME/.gitconfig-work"
+[user]
+	email = $work_email
+  signingkey = $HOME/.ssh/id_ed25519_sk_work.pub
+EOF
+fi
